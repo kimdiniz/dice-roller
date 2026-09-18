0f3df0df2564f5bd50ff8fb1622103afff78510c
kimdiniz <kimberlydiniz23@gmail.com>
2026-09-18 15:04:59 +0100

    Create dice_roller.py

---
+++ dice_roller.py
@@ -0,0 +1,12 @@
+import random
+
+while True:
+  input("Press enter to roll the dice!")
+
+dice = random.randint(1, 6)
+print("You rolled:", dice)
+
+again = input("Roll again? (Yes/No): ")
+
+if.again.lower() != "yes":
+  break
